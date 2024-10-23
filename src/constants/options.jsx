6ea27e698c2bfc2c7df2852a378ab66f45164ae0
kritii07 <kritiantwal.7@gmail.com>
2024-10-23 23:31:28 +0530

    AIModal Changes

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -50,4 +50,4 @@ export const SelectBudgetOptions=[
     }
 ]
 
-export const AI_PROMPT = 'Generate Travel Plan for Location : {location}, for {totalDays} Days for {traveler} with a {budget} budget, give me Hotels options list with HotelName, Hotel address, Price, Hotel image url, geo coordinates, ticket Pricing, Time travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format.'
+export const AI_PROMPT = 'Generate Travel Plan for Location: {location}, for {totalDays} days for {traveler} with a {budget} budget. Give me a Hotels options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions and suggest itinerary with placeName, place Details, Place Image Url, Geo Coordinates, ticket Pricing, rating, Time Travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format.'

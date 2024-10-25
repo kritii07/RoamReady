698eebf12250bca32b6f229a40be4ff8b23a8617
kritii07 <kritiantwal.7@gmail.com>
2024-10-25 13:54:18 +0530

    image added forplaces

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -1,11 +1,33 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalApi';
+import React, { useEffect, useState } from 'react'
 import { IoIosSend } from "react-icons/io";
 
+
 function InfoSection({trip}) {
+
+  const [photoUrl,setPhotoUrl] = useState();
+
+  const GetPlacePhoto = async() => {
+    const data = {
+      query:trip?.userSelection?.location?.label
+    }
+    const result = await GetPlaceDetails(data).then(resp=>{
+      //console.log(resp.data.places[0].photos[4].name);
+
+      const PhotoUrl = PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name);
+      //console.log(PhotoUrl);
+      setPhotoUrl(PhotoUrl);
+    })
+  }
+
+  useEffect(()=>{
+    trip && GetPlacePhoto();
+  },[trip])
+
   return (
     <div>
-        <img src="/placeholder.jpeg" className='h-[340px] w-full object-cover rounded-xl' />
+        <img src={photoUrl?photoUrl:"/placeholder.jpeg"} className='h-[340px] w-full object-cover rounded-xl' />
 
         <div className='flex justify-between items-center'>
             {/* place information  */}
--- src/view-trip/components/PlaceCardItem.jsx
+++ src/view-trip/components/PlaceCardItem.jsx
@@ -1,13 +1,34 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalApi';
+import React, { useState, useEffect } from 'react'
 import { FaMapLocationDot } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 function PlaceCardItem({place}) {
+
+  const [photoUrl,setPhotoUrl] = useState();
+
+    const GetPlacePhoto = async() => {
+      const data = { 
+        textQuery:place.placeName
+      }
+      const result = await GetPlaceDetails(data).then(resp=>{
+        //console.log(resp.data.places[0].photos[4].name);
+  
+        const PhotoUrl = PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name);
+        //console.log(PhotoUrl);
+        setPhotoUrl(PhotoUrl);
+      })
+    }
+  
+    useEffect(()=>{
+      place && GetPlacePhoto();
+    },[place])
+
   return (
     <Link to={'https://www.google.com/maps/search/?api=1&query='+place.placeName} target='_blank' >
         <div className='border rounded-xl p-3 mt-2 flex gap-5 hover:scale-105 transition-all cursor-pointer hover:shadow-md'>
-            <img src="/placeholder.jpeg" className='w-[130px] h-[130px] rounded-xl' />
+            <img src={photoUrl?photoUrl:"/placeholder.jpeg"} className='w-[130px] h-[130px] rounded-xl object-cover' />
             <div>
                 <h2 className='font-bold text-lg'>{place.placeName}</h2>
                 <p className='text-sm text-gray-400'>{place.placeDetails}</p>

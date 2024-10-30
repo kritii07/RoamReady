c9f3a395d5faf23c02e73fc50e020a770f3099a7
kritii07 <kritiantwal.7@gmail.com>
2024-10-30 22:07:24 +0530

    home page bg image added

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
+import backgroundImage from "/heroSection.jpg"; // Adjust the path as needed
 
 function Hero() {
   return (
-    <div className="flex flex-col items-center mx-56 gap-9">
-      <h1 className="font-extrabold text-[50px] text-center mt-16">
-        <span className="text-[#f56551]">
-          Discover your next adventure with ai:
-        </span>{" "}
-        personalised your trips
+    <div
+      className="flex flex-col items-center justify-start pt-16 gap-9 h-screen w-full bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <h1 className="font-extrabold text-[50px] text-[white] text-center mt-16">
+        Your Next Journey, Optimized
       </h1>
 
-      <p className="text-xl text-gray-500 text-center">
-        {" "}
-        your personal trip planner and travel curator
+      <p className="text-xl font-semibold text-center w-[700px] mx-auto  ">
+        Build, personalize, and optimize your itineraries with our free AI trip planner. Designed for vacations and everyday adventures.
       </p>
 
       <Link to={"/create-trip"}>
-        <Button>get started, its free</Button>
+        <Button className="rounded-full mt-[20px] w-[200px] h-[60px] text-md">Get started, it's free</Button>
       </Link>
     </div>
   );

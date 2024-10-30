8d399ae19582888e69f40a0f928a491cafcf2070
kritii07 <kritiantwal.7@gmail.com>
2024-10-30 22:04:24 +0530

    user's all trips

---
+++ src/my-trips/components/UserTripCardItem.jsx
@@ -0,0 +1,43 @@
+import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalApi';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function UserTripCardItem({trip}) {
+
+    const [photoUrl,setPhotoUrl] = useState();
+
+    useEffect(()=>{
+        trip && GetPlacePhoto();
+      },[trip])
+
+    const GetPlacePhoto = async() => {
+      const data = {
+        textQuery:trip?.userSelection?.location?.label
+      }
+      const result = await GetPlaceDetails(data).then(resp=>{
+        //console.log(resp.data.places[0].photos[4].name);
+  
+        const PhotoUrl = PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[1].name);
+        //console.log(PhotoUrl);
+        setPhotoUrl(PhotoUrl);
+      })
+    }
+  
+    
+  
+  return (
+    <Link to={'/view-trip/'+trip?.id}>
+    <div className='hover:scale-105 transition-all '>
+        <img src={photoUrl?photoUrl:"/placeholder.jpeg"} 
+        className='object-cover rounded-xl h-[200px] w-[290px]' />
+
+        <div>
+            <h2 className='font-bold text-lg w-[290px]'>{trip?.userSelection?.location?.label}</h2>
+            <h2 className='text-sm text-gray-500'>{trip?.userSelection?.noOfDays} Days trip with {trip?.userSelection?.budget} Budget</h2>
+        </div>
+    </div>
+    </Link>
+  )
+}
+
+export default UserTripCardItem
